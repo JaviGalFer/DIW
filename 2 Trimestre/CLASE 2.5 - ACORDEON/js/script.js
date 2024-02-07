c56725d89872fe2ceed48b48ac1c690501e53a17
JaviGalFer <fgalfer299@g.educaand.es>
2024-02-07 20:30:03 +0100

    update

---
+++ 2 Trimestre/CLASE 2.5 - ACORDEON/js/script.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const carrusel = document.querySelector(".carrusel");
+    const slides = carrusel.querySelectorAll(".slide");
+    const totalSlides = slides.length;
+    const btnLeft = carrusel.querySelector(".btn--left");
+    const btnRight = carrusel.querySelector(".btn--right");
+    const dots = carrusel.querySelectorAll(".dot");
+    let currentSlide = 0;
+
+// Función para actualizar la visualización del carrusel
+function updateCarrusel() {
+    slides.forEach((slide, index) => {
+    slide.style.display = index === currentSlide ? "block" : "none";
+    });
+
+    dots.forEach((dot, index) => {
+    dot.classList.toggle("dot--fill", index === currentSlide);
+    });
+}
+
+// Función para manejar el clic en el botón izquierdo
+btnLeft.addEventListener("click", function () {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    updateCarrusel();
+});
+
+// Función para manejar el clic en el botón derecho
+btnRight.addEventListener("click", function () {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    updateCarrusel();
+});
+
+// Función para manejar el clic en los dots
+dots.forEach((dot, index) => {
+    dot.addEventListener("click", function () {
+    currentSlide = index;
+    updateCarrusel();
+    });
+});
+
+// Inicializar el carrusel
+updateCarrusel();
+});
